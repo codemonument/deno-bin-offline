6e790b49cacc2f01dfd6fc4b58273654af7ea145
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-04 12:18:36 +0100

    add progress handler in downloadCI

--- src/builder/downloadCI.ts
+++ src/builder/downloadCI.ts
@@ -3,11 +3,22 @@ import axios from "axios";
 
 const pkgVersion = process.env.npm_package_version;
 
+const onDownloadProgress = (name) => (progressEvent) => {
+  // console.log(progressEvent);
+  const percentCompleted = progressEvent.progress.toFixed(2);
+  console.log(` ${name} - download completed:`, percentCompleted);
+};
+
 export async function downloadCI() {
   const downloadPromises = denoZips.map(async (denoVariant) => {
     const dlUrl =
       `https://github.com/denoland/deno/releases/download/v${pkgVersion}/${denoVariant.zipName}`;
-    await axios.get(dlUrl);
+
+    console.debug(dlUrl);
+
+    const name = `${denoVariant.platform}-${denoVariant.arch}`;
+
+    await axios.get(dlUrl, { onDownloadProgress: onDownloadProgress(name) });
   });
 
   await Promise.all(downloadPromises);
