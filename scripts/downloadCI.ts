bcadd7a08ccd1e5e90709ac672a29db66e9fd6bf
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-04 12:01:28 +0100

    add basic skeleton to downloadCI.ts script

---
+++ scripts/downloadCI.ts
@@ -0,0 +1,14 @@
+import { denoZips } from "../src/denoZips";
+import axios from "axios";
+
+const pkgVersion = process.env.npm_package_version;
+
+export async function downloadCI() {
+  const downloadPromises = denoZips.map(async (denoVariant) => {
+    const dlUrl =
+      `https://github.com/denoland/deno/releases/download/v${pkgVersion}/${denoVariant.zipName}`;
+    await axios.get(dlUrl);
+  });
+
+  await Promise.all(downloadPromises);
+}
