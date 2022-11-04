4bb6ee84ede9346fbd9a7a6c284068af266e72cf
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-04 11:49:43 +0100

    convert runtimeVariants to denoExecutables

--- src/denoExecutables.ts
+++ src/denoExecutables.ts
@@ -6,54 +6,41 @@
  * key of map: `${platform}-${arch}` for faster lookup
  */
 
-const runtimeVariants = new Map(
-  [
+export const denoExecutables = new Map(
+  [[
     "win32-x64",
     {
       platform: "win32",
       arch: "x64",
-      zipName: "deno-x86_64-pc-windows-msvc.zip",
       executableName: "deno.exe",
     },
-  ],
-  [
+  ], [
     "darwin-x64",
     {
       platform: "darwin",
       arch: "x64",
-      zipName: "deno-x86_64-apple-darwin.zip",
       executableName: "deno",
     },
-  ],
-  [
+  ], [
     "darwin-arm64",
     {
       platform: "darwin",
       arch: "arm64",
-      zipName: "deno-aarch64-apple-darwin.zip",
       executableName: "deno",
     },
-  ],
-  [
+  ], [
     "linux-x32",
     {
       platform: "linux",
       arch: "x32",
-      zipName: "deno-x86_64-unknown-linux-gnu.zip",
       executableName: "deno",
     },
-  ],
-  [
+  ], [
     "linux-x64",
     {
       platform: "linux",
       arch: "x64",
-      zipName: "deno-x86_64-unknown-linux-gnu.zip",
       executableName: "deno",
     },
-  ]
+  ]],
 );
-
-module.exports = {
-  runtimeVariants,
-};
