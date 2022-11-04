56f9ba536197d42bf51f35acc526bac55b93f77d
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-04 11:17:07 +0100

    add list of deno zips to download

---
+++ src/denoZips.ts
@@ -0,0 +1,18 @@
+export const denoZips = [
+  { platform: "darwin", arch: "x64", zipName: "deno-x86_64-apple-darwin.zip" },
+  {
+    platform: "win32",
+    arch: "x64",
+    zipName: "deno-x86_64-pc-windows-msvc.zip",
+  },
+  {
+    platform: "darwin",
+    arch: "arm64",
+    zipName: "deno-aarch64-apple-darwin.zip",
+  },
+  {
+    platform: "linux",
+    arch: "x32",
+    zipName: "deno-x86_64-unknown-linux-gnu.zip",
+  },
+];
