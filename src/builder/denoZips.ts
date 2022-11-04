306430a5193831486d1a69bb0b285852e98d05cf
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-04 13:31:44 +0100

    add executable name to deno zips

--- src/builder/denoZips.ts
+++ src/builder/denoZips.ts
@@ -1,18 +1,26 @@
 export const denoZips = [
-  { platform: "darwin", arch: "x64", zipName: "deno-x86_64-apple-darwin.zip" },
   {
     platform: "win32",
     arch: "x64",
     zipName: "deno-x86_64-pc-windows-msvc.zip",
+    executableName: "deno.exe",
+  },
+  {
+    platform: "darwin",
+    arch: "x64",
+    zipName: "deno-x86_64-apple-darwin.zip",
+    executableName: "deno",
   },
   {
     platform: "darwin",
     arch: "arm64",
     zipName: "deno-aarch64-apple-darwin.zip",
+    executableName: "deno",
   },
   {
     platform: "linux",
     arch: "x86_64",
     zipName: "deno-x86_64-unknown-linux-gnu.zip",
+    executableName: "deno",
   },
 ];
