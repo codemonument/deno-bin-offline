2e9008efbc7ba980361f2251744f316051d0673a
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-05 13:40:38 +0100

    add testing deps to import_map

---
+++ src/deps/testing.std.ts
@@ -0,0 +1,4 @@
+export * from "https://deno.land/std@0.162.0/testing/bdd.ts";
+export * from "https://deno.land/std@0.162.0/testing/snapshot.ts";
+export * from "https://deno.land/std@0.162.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.162.0/async/deadline.ts";
