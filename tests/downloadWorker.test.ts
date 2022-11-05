f46ed8589bbf37f21022318ad9c03598ab36a0cc
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-05 14:36:40 +0100

    simplify downlodWorkerTest

--- tests/downloadWorker.test.ts
+++ tests/downloadWorker.test.ts
@@ -2,26 +2,22 @@ import { assert } from "testing.std";
 import * as Comlink from "comlink";
 
 Deno.test(`downloadWorker.ts`, async (ts) => {
-  await ts.step(`Can create worker from import_map.json`, async () => {
-    const worker = new Worker(
-      new URL("../src/builder/downloadWorker.ts", import.meta.url).href,
-      {
-        type: "module",
-      },
-    );
-    assert(worker);
+  const workerURL =
+    new URL("../src/builder/downloadWorker.ts", import.meta.url).href;
+  const workerOptions: WorkerOptions = {
+    type: "module",
+  };
 
+  await ts.step(`Can create worker from import_map.json`, async () => {
+    const worker = new Worker(workerURL, workerOptions);
     const comlinkObj = Comlink.wrap(worker);
 
-    // Fails bc. it can't pack the function into a ppostMessage body, but since this error arrives,
-    // i know that the Comlink wrapper works
+    assert(worker);
     assert(await comlinkObj.comlinkReady);
-
-    // console.log(worker);
 
     worker.terminate();
   });
-  // await ts.step(`Can get progress from worker!`, async () => {
 
-  // });
+  await ts.step(`Can get progress from worker!`, async () => {
+  });
 });
