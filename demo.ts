bb13bf62c0abdb1f28b41b6f3763fbfbb22190dd
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-06 09:31:04 +0100

    update demo.ts for zip extraction

--- demo.ts
+++ demo.ts
@@ -1,5 +1,11 @@
-import { downloadCI } from "./src/builder/downloadCI";
+import { ZipReader } from "zipjs";
 
-downloadCI().then(() => {
-  console.log("Download successful!");
-}).catch((error) => console.log(`download error`, error));
+const zipPath =
+  "/Users/bjesuiter/Develop/codemonument/deno-bin-offline/dist/bin/darwin/arm64/deno-aarch64-apple-darwin.zip";
+
+const zipFile = await Deno.open(zipPath, { read: true });
+const zipReader = new ZipReader(zipFile.readable);
+
+const entries = await zipReader.getEntries();
+
+console.log(entries);
