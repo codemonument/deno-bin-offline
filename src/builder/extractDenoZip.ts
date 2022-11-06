9ce5616779029e7188c39166c151fa90ba1b3cc5
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-06 08:57:46 +0100

    add ExtractDenoZipResult

--- src/builder/extractDenoZip.ts
+++ src/builder/extractDenoZip.ts
@@ -3,17 +3,30 @@ import { decompress } from "zip";
 import { join } from "path.std";
 import { ensureDir } from "fs.std";
 
+export type ExtractDenoZipResult = {
+  outPath: string;
+  filePath: string;
+  filename: string;
+};
+
 export async function extractDenoZip(variant: DenoVariant, zipPath: string) {
   const filename = variant.executableName;
   const outPath = join("dist", "bin", variant.platform, variant.arch);
+  const filePath = join(outPath, filename);
   await ensureDir(outPath);
 
   // 3. Extracts `deno` entry to  folder
   decompress(zipPath, outPath, { includeFileName: true });
   // 4. Changes the file permission
   if (variant.platform !== "win32") {
-    await Deno.chmod(join(outPath, filename), 0o755);
+    await Deno.chmod(filePath, 0o755);
   }
   // 5. Removes the zip file
   await Deno.remove(zipPath);
+
+  return {
+    outPath,
+    filePath,
+    filename,
+  };
 }
