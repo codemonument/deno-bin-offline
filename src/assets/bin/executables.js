21e54096d5a4252483ef825550978acd439ec4f8
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-06 13:27:12 +0100

    convert denoExecutables to executables.js  + added it to asset copy

--- src/assets/bin/executables.js
+++ src/assets/bin/executables.js
@@ -6,41 +6,45 @@
  * key of map: `${platform}-${arch}` for faster lookup
  */
 
-export const denoExecutables = new Map(
-  [[
+export const denoExecutables = new Map([
+  [
     "win32-x64",
     {
       platform: "win32",
       arch: "x64",
       executableName: "deno.exe",
     },
-  ], [
+  ],
+  [
     "darwin-x64",
     {
       platform: "darwin",
       arch: "x64",
       executableName: "deno",
     },
-  ], [
+  ],
+  [
     "darwin-arm64",
     {
       platform: "darwin",
       arch: "arm64",
       executableName: "deno",
     },
-  ], [
+  ],
+  [
     "linux-x32",
     {
       platform: "linux",
-      arch: "x32",
+      arch: "x86_64",
       executableName: "deno",
     },
-  ], [
+  ],
+  [
     "linux-x64",
     {
       platform: "linux",
-      arch: "x64",
+      arch: "x86_64",
       executableName: "deno",
     },
-  ]],
-);
+  ],
+]);
--- src/features/copy-assets/task.ts
+++ src/features/copy-assets/task.ts
@@ -1,6 +1,7 @@
 export async function copyAssets() {
   await Deno.copyFile(`./src/assets/index.js`, `./dist/index.js`);
   await Deno.copyFile(`./src/assets/bin/deno.js`, `./dist/bin/deno.js`);
+  await Deno.copyFile(`./src/assets/bin/deno.js`, `./dist/bin/deno.js`);
 
   console.info(`Copied assets!`);
 }
