ec19484af44c72668fe40a27af92cb52c4d26175
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-06 13:46:33 +0100

    implement new deno.js with executable selection

--- assets/bin/deno.js
+++ assets/bin/deno.js
@@ -2,17 +2,22 @@
 
 import fs from "node:fs";
 import { join } from "node:path";
-import child_process from "node:child_process";
 import { denoExecutables } from "./executables";
+import child_process from "node:child_process";
 
 (async function () {
   const executable = denoExecutables.get(`${process.platform}-${process.arch}`);
 
-  const executableFilename = process.platform === "win32" ? "deno.exe" : "deno";
+  let executablePath = join(
+    __dirname,
+    executable.platform,
+    executable.arch,
+    executable.executableFilename
+  );
 
-  let executablePath = path.join(__dirname, executableFilename);
-
-  if (!fs.existsSync(executablePath)) await require("../install.js");
+  if (!fs.existsSync(executablePath))
+    throw new Error(`Deno Executable not found at ${executablePath}. Something is wrong with this install.
+  Please raise an issue at: https://github.com/codemonument/deno-bin-offline/issues`);
 
   const p = child_process.spawnSync(executablePath, process.argv.slice(2), {
     cwd: process.cwd(),
