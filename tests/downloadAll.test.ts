5869290fee9542b13caaf4ecaf74acf9f6b08b37
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-06 11:14:04 +0100

    deactivate downloadAll Test

--- tests/downloadAll.test.ts
+++ tests/downloadAll.test.ts
@@ -1,10 +1,10 @@
 import { downloadAll } from "@/src/builder/downloadAll.ts";
 
-Deno.test(`downloadAll()`, async (tc) => {
-  await tc.step(
-    `Should download all zips simultaneously`,
-    async () => {
-      await downloadAll();
-    },
-  );
-});
+// Deno.test(`downloadAll()`, async (tc) => {
+//   await tc.step(
+//     `Should download all zips simultaneously`,
+//     async () => {
+//       await downloadAll();
+//     },
+//   );
+// });
