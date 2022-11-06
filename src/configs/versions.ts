371d3a0786916e56a94a23bed17cc9bb25746809
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-06 15:42:24 +0100

    read versions from versions.json

--- src/configs/versions.ts
+++ src/configs/versions.ts
@@ -1,8 +1,13 @@
-await Deno;
+const versionsText = await Deno.readTextFile(`../../versions.json`);
+const json: {
+  DENO_VERSION: string;
+  TARGET_PACKAGE_SUFFIX?: string;
+} = JSON.parse(versionsText);
 
 // add a prerelease id like this: 1.27.1-beta.1
-export const DENO_VERSION = "1.27.1";
-export const TARGET_PACKAGE_VERSION = `${DENO_VERSION}-local.1`;
+export const DENO_VERSION = json.DENO_VERSION;
+export const TARGET_PACKAGE_VERSION =
+  `${DENO_VERSION}${json.TARGET_PACKAGE_SUFFIX}`;
 
 // Add a version alias to not have to change all the sourcecode
 export const VERSION = DENO_VERSION;
