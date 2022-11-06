a97704baf399bc072d1475db9e84afd1a09a5e87
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-06 12:12:44 +0100

    finish implementation of downloadAll with returning an array of out file paths

--- src/builder/downloadAll.ts
+++ src/builder/downloadAll.ts
@@ -1,6 +1,9 @@
 import { VERSION } from "@version";
 import { denoZips } from "./denoZips.ts";
 import { downloadVariant } from "@/src/builder/downloadVariant.ts";
+import { join, resolve } from "path.std";
+import { terminateWorkers } from "zipjs";
+import { extractDenoZip } from "./extractDenoZip.ts";
 
 export async function downloadAll() {
   // TODO: Activate this parallel downloading when parallel progressbars are ready & possible
@@ -9,13 +12,19 @@ export async function downloadAll() {
   // });
   // return Promise.all(downloadPromises);
 
-  const variant1 = denoZips[0];
-  const variant2 = denoZips[1];
-  const variant3 = denoZips[2];
-  const variant4 = denoZips[3];
+  // Note: This will download and extract each zip one at a time (because each for-loop iteration will be awaited)
+  const outFilePaths = [];
+  for (const variant of denoZips) {
+    const { zipPath } = await downloadVariant(variant, VERSION);
 
-  const res1 = await downloadVariant(variant1, VERSION);
-  const res2 = await downloadVariant(variant2, VERSION);
-  const res3 = await downloadVariant(variant3, VERSION);
-  const res4 = await downloadVariant(variant4, VERSION);
+    const outDir = resolve(
+      join("dist", "bin", variant.platform, variant.arch),
+    );
+    const { outFilePath } = await extractDenoZip(variant, zipPath, outDir);
+    outFilePaths.push(outFilePath);
+  }
+
+  terminateWorkers();
+
+  return outFilePaths;
 }
