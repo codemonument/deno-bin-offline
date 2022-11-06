3c90be9b34f04221b286038267552b25d97ce882
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-06 09:51:19 +0100

    remove temp zip in downlodVariant.test

--- tests/downloadVariant.test.ts
+++ tests/downloadVariant.test.ts
@@ -4,12 +4,14 @@ Deno.test(`downloadVariant()`, async (tc) => {
   await tc.step(
     `Should download the zip and output progress simultaneously`,
     async () => {
-      await downloadVariant({
+      const { zipPath } = await downloadVariant({
         platform: "darwin",
         arch: "x64",
         zipName: "deno-x86_64-apple-darwin.zip",
         executableName: "deno",
       }, "1.27.1");
+
+      await Deno.remove(zipPath);
     },
   );
 });
