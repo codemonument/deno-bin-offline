681b15dbac0eadcc05b1a4d0e56d777684ec55af
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-06 13:27:20 +0100

    fix copy path

--- src/features/copy-assets/task.ts
+++ src/features/copy-assets/task.ts
@@ -1,7 +1,10 @@
 export async function copyAssets() {
   await Deno.copyFile(`./src/assets/index.js`, `./dist/index.js`);
   await Deno.copyFile(`./src/assets/bin/deno.js`, `./dist/bin/deno.js`);
-  await Deno.copyFile(`./src/assets/bin/deno.js`, `./dist/bin/deno.js`);
+  await Deno.copyFile(
+    `./src/assets/bin/executables.js`,
+    `./dist/bin/executables.js`,
+  );
 
   console.info(`Copied assets!`);
 }
