41ca20d270c688778e502c75cf1a18d29e44b75b
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-06 13:04:54 +0100

    add more details to packagejson template file

--- src/features/package-json/template.ts
+++ src/features/package-json/template.ts
@@ -5,15 +5,35 @@ export const packageJson = {
   version: VERSION,
   description:
     "An inofficial distribution of the deno binary, a secure runtime for JavaScript and TypeScript (Offline-Install)",
-  main: "index.js",
-  "files": [
-    "index.js",
+  type: "module",
+  // Not needded for type module
+  // main: "index.js",
+  // Note: Automatically added files:
+  // main script (if applicable), Readme.md, package.json
+  files: [
     "bin",
   ],
-  "bin": {
+  bin: {
     "deno": "./bin/deno.js",
     "deno-bin-offline": "./bin/deno.js",
   },
+  repository: {
+    type: "git",
+    url: "git+https://github.com/codemonument/deno-bin-offline.git",
+  },
+  keywords: [
+    "deno",
+  ],
+  author: "Benjamin Jesuiter",
+  license: "MIT",
+  bugs: {
+    url: "https://github.com/codemonument/deno-bin-offline/issues",
+  },
+  homepage: "https://github.com/codemonument/deno-bin-offline#readme",
+  dependencies: {
+    "adm-zip": "^0.5.4",
+  },
+  devDependencies: {},
 };
 
 export function renderPackageJson(outPath?: string) {
