03bba3cbe9c1c3c157b4ee17b598b0892e2a038a
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-06 14:02:00 +0100

    add option to skip downloading (for faster iterations locally)

--- src/builder.ts
+++ src/builder.ts
@@ -2,13 +2,33 @@ import { downloadAll } from "@/src/features/deno-downloads/downloadAll.ts";
 import { copyAssets } from "@/src/features/copy-assets/task.ts";
 import { renderPackageJson } from "@/src/features/package-json/template.ts";
 
-async function main() {
-  await downloadAll();
+export type BuilderOptions = {
+  skipDownload: boolean;
+};
+
+const defaultOptions: BuilderOptions = {
+  skipDownload: false,
+};
+
+async function main(options?: BuilderOptions) {
+  options = (!options) ? defaultOptions : { ...defaultOptions, ...options };
+  const { skipDownload } = options;
+
+  if (!skipDownload) {
+    await downloadAll();
+  } else {
+    console.info(`Skipped downloading due to --skipDownload flag`);
+  }
   await copyAssets();
   await renderPackageJson();
 }
 
-main().then(() => {
+const options: BuilderOptions = defaultOptions;
+if (Deno.args[0] === "--skipDownload") {
+  options.skipDownload = true;
+}
+
+main(options).then(() => {
   console.log("Dist Package Setup finished");
 }).catch((error) =>
   console.log(`Error while setting up the target package in dist!`, error)
