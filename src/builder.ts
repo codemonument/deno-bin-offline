13da8308d30e8f41a5acb110e065eb2dcff5192e
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-06 13:17:31 +0100

    add copyAssets and renderPackageJson to builder.ts

---
+++ src/builder.ts
@@ -0,0 +1,15 @@
+import { downloadAll } from "@/src/features/deno-downloads/downloadAll.ts";
+import { copyAssets } from "@/src/features/copy-assets/task.ts";
+import { renderPackageJson } from "@/src/features/package-json/template.ts";
+
+async function main() {
+  await downloadAll();
+  await copyAssets();
+  renderPackageJson();
+}
+
+main().then(() => {
+  console.log("Dist Package Setup finished");
+}).catch((error) =>
+  console.log(`Error while setting up the target package in dist!`, error)
+);
