1e252f8f8acd34f69d380cf72dbf55abad952083
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-06 12:20:54 +0100

    fix test for downloadAll

--- tests/downloadAll.test.ts
+++ tests/downloadAll.test.ts
@@ -1,10 +1,16 @@
 import { downloadAll } from "@/src/builder/downloadAll.ts";
+import { assert } from "testing.std";
 
-// Deno.test(`downloadAll()`, async (tc) => {
-//   await tc.step(
-//     `Should download all zips simultaneously`,
-//     async () => {
-//       await downloadAll();
-//     },
-//   );
-// });
+Deno.test(`downloadAll()`, async (tc) => {
+  await tc.step(
+    `Should download all zips`,
+    async () => {
+      const outPaths = await downloadAll();
+      for (const path of outPaths) {
+        console.log(`LSTAT for `, path);
+        const stats = await Deno.lstat(path);
+        assert(stats.isFile);
+      }
+    },
+  );
+});
